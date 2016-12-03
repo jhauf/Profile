5c37856132f444655b3141b3330218bd91061942
Jane Hauf <janehauf@gmail.com>
2016-12-03 10:53:50 -0800

    add default

--- assets/js/util.js
+++ assets/js/util.js
@@ -157,25 +157,8 @@
 								return;
 
 							// Cancel original event.
-								event.preventDefault();
-								event.stopPropagation();
 
-							// Hide panel.
-								$this._hide();
-
-							// Redirect to href.
-								window.setTimeout(function() {
-
-									if (target == '_blank')
-										window.open(href);
-									else
-										window.location.href = href;
-
-								}, config.delay + 10);
-
-						});
-
-				}
+				
 
 			// Event: Touch stuff.
 				$this.on('touchstart', function(event) {
